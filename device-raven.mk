c0dc9f2293a79fb3f5ee7b4e415386b2e43d22f6
pimpmaneaton <shaunsibu@gmail.com>
2024-09-22 02:48:11 -0600

    Fix build errors

--- device-raven.mk
+++ device-raven.mk
@@ -251,8 +251,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 
 # Keyboard side padding in dp for portrait mode
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_r=11
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_l=11
+# PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_r=11
+# PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_l=11
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
